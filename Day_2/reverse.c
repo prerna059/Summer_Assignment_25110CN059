f3039fb08d7fcc5e9aaa9fdbc3b7eaec88dd0a82
prerna059 <prernalodhi2006@gmail.com>
2026-06-02 23:04:08 +0530

    Update reverse.c

--- Day_2/reverse.c
+++ Day_2/reverse.c
@@ -5,7 +5,7 @@ int main()
     int n, r, reverse=0;
     printf("Enter the number :");
     scanf("%d", &n);
-    while(n>0)//Correction: n!=0
+    while(n>0)//Correction: n!=0 as negative case is also included by doin dis
     {
         r=n%10;
         reverse= reverse*10+r;

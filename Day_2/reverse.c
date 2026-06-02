2f93bf0183a122692009a65aee9a44b5ebb9e4f6
prerna059 <prernalodhi2006@gmail.com>
2026-06-02 21:29:41 +0530

    Add files via upload

---
+++ Day_2/reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n, r, reverse=0;
+    printf("Enter the number :");
+    scanf("%d", &n);
+    while(n>0)
+    {
+        r=n%10;
+        reverse= reverse*10+r;
+        n=n/10;
+    }
+    printf("The reverse of the number is %d",reverse);
+    return 0;
+}

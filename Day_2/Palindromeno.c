416207fd9ef3035f80f7e8303e3f1a847631ceb4
prerna059 <prernalodhi2006@gmail.com>
2026-06-02 21:30:25 +0530

    Add files via upload

---
+++ Day_2/Palindromeno.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n, r, reverse = 0;
+    printf("Enter the number :");
+    scanf("%d", &n);
+    int number=n;
+    while(n>0)
+    {
+        r=n%10;
+        reverse= reverse*10+r;
+        n=n/10;
+    }
+    if(number==reverse)
+    {
+        printf("The number is palindrome number.");
+    }
+    else 
+    {
+        printf("The given number is not a palindrome number.");
+    }
+    return 0;
+}
+

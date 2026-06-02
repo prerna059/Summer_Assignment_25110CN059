19191cdf6e45ed451a2ca57a35d8b7605db9be0b
prerna059 <prernalodhi2006@gmail.com>
2026-06-02 21:28:19 +0530

    Add files via upload

---
+++ Day_2/digitsum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n, r, sum=0;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+      r=n%10;
+      sum=sum+r;
+      n=n/10;
+    }
+    printf("The sum of the digits of the number is: %d", sum);
+    return 0;
+}

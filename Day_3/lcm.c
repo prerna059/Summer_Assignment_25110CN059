c99be340c4d9635d0a5f849b8e6955ddf60e5dd9
prerna059 <prernalodhi2006@gmail.com>
2026-06-03 20:42:34 +0530

    Add files via upload

---
+++ Day_3/lcm.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int gcd, a, b;
+    printf("enter two numbers: ");
+    scanf("%d%d",&a,&b);
+    int min = (a>b)?b:a;
+    gcd=1;
+    for (int i=1; i<=min; i++)
+    {
+        if(a%i==0 && b%i==0){
+            gcd=i;
+        }
+    }
+    int lcm = a*b/gcd;
+    printf("The lcm of two numbers is %d", lcm);
+    return 0;
+}

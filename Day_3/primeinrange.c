1566ed4cd7daf63db627f56fe7265d54c208c1a7
prerna059 <prernalodhi2006@gmail.com>
2026-06-03 16:41:20 +0530

    Update primeinrange.c

--- Day_3/primeinrange.c
+++ Day_3/primeinrange.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+int  l,u, flag = 0;
+printf("Enter lower limit:");
+scanf("%d",&l);
+printf("Enter upper limit:");
+scanf("%d", &u);
+int temp=u;
+if(l>u){
+    u=l;
+    l=temp;
+}
+int p;
 
+for (int n=l; n<=u; n=n+1)
+{

65b20c9a5c435384f2d0759e93ed69e4877d7fd8
prerna059 <prernalodhi2006@gmail.com>
2026-06-03 15:17:01 +0530

    Add files via upload

--- Day_3/prime.c
+++ Day_3/prime.c
@@ -1,1 +1,43 @@
 
+#include<stdio.h>
+ 
+int main()
+{
+int  n, flag = 0;
+printf("Enter a number to be checked ");
+scanf("%d",&n);
+
+if(n<=1)
+{
+    printf("%d is not prime", n);
+}
+
+else if (n>1)
+ {
+   for(int i =1 ; i<=n; i++)
+    {
+        if (n%i==0){
+           flag++;
+        }
+
+    }
+
+  if (flag > 2)
+  {
+     printf("%d is not prime", n);
+  }
+  else
+  {
+     printf("%d is a prime number", n);
+  }
+ }
+else 
+{
+    printf("Entered number is not valid");
+}
+  return 0;
+}
+
+
+
+  

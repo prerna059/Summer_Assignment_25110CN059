08187bac929b4565d9e199d12eda11ed8044a17d
prerna059 <prernalodhi2006@gmail.com>
2026-06-05 14:21:56 +0530

    Add files via upload

---
+++ Day_4/Armstrong.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n, r, count=0;
+    long int sum=0;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    int temp=n;//using loop further in the code will mannipulate the entered value for n
+    if(n==0){
+        count=1;
+    }
+    while(n!=0)
+    {
+        n=n/10;
+        count++;
+    }
+    n=temp;
+    while(n!=0)
+    {
+        r=n%10;
+        sum= sum+pow(r,count);
+        n=n/10;
+    }
+    n=temp;
+    if(sum==n){
+        printf("The entered number is armstrong");
+    }
+else{
+    printf("The entered number is not an Armstrong number");
+}
+return 0;
+}

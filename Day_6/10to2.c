52978cac9dba5cb9454ebcc9e002fff3e9205925
prerna059 <prernalodhi2006@gmail.com>
2026-06-06 20:44:26 +0530

    Update 10to2.c

--- Day_6/10to2.c
+++ Day_6/10to2.c
@@ -4,25 +4,56 @@ int main(){
     int n;
     printf("Enter a decimal number: ");
     scanf("%d", &n);
-    int temp=n;
     int temp2=n;
+    n=(n<0)?-n:n;
+    int temp=n;
+    
     int count=0;
+    if(n==0){
+        printf("The binary form of 0 is 0");
+    }
     while(temp!=0)
     {
      int r=temp%2;
      temp/=2;
      count++;
     }
-    int binary[count-1];
+    int binary[count];
    for(int i=count-1; i>=0;i--){
     binary[i]=n%2;
-    n/=n;
+    n=(n-binary[i])/2;
    }
+    
     n=temp2;
     printf("The binary form of %d is ", n);
+    /*if(n<0){
+        int negbinary[count];
+        for(int k=0;k<=count-1;k++){
+       negbinary[k]= ~binary[k];
+        }
+        if(negbinary[count-1]==0){
+        negbinary[count-1]+1;
+       }
+       else{
+        int i;
+        //while(i!=0)
+        for(i=count-1;i>=0;i--){
+           if(negbinary!=0) {
+           negbinary[i]=1;
+            
+           }
+           
+        
+       }
+    }
+}
+   
+else{*/
     for(int j=0; j<=count-1; j++){
         printf("%d", binary[j]);
     }
+
     
     return 0;
 }
+

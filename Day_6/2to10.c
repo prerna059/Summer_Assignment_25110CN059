f4d5d4efd94c6eba997a3b1010e4eb5e4fed107c
prerna059 <prernalodhi2006@gmail.com>
2026-06-06 21:39:06 +0530

    Update 2to10.c

--- Day_6/2to10.c
+++ Day_6/2to10.c
@@ -1,1 +1,19 @@
 
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n;
+    printf("Enter a binary number: ");
+    scanf("%d", &n);
+    int temp2=n;
+    int decimal=0, b=1;
+     while (n!=0) {
+        int r=n%10;     
+        decimal=r*b+decimal;  
+        b=b*2;              
+        n=n/10;         
+    }
+    n=temp2;
+    printf("The decimal form of %d is %d", n, decimal);
+    return 0;
+}

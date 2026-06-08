b5d55d2edf38040d0a78a2f3c6c403e34655f58a
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-08 13:36:11 +0530

    Update repeated_no_triangle.c

--- Day_8/repeated_no_triangle.c
+++ Day_8/repeated_no_triangle.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+  int main()
+{
+  int rows;
+printf("Enter the postive number of rows: ");
+scanf("%d", &rows);
+rows=(rows<0)?-rows:rows;  
+for(int i=1; i<=rows; i++){
+for(int j=1; j<=i;j++){
+printf("%d", i);
+}
+printf("\n");
+}
+return 0;
+}
 

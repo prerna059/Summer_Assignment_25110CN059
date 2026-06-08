3c99a483cac2350f9a5df2f208636f5138e86fd9
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-08 15:31:42 +0530

    Update char_triangle.c

--- Day_8/char_triangle.c
+++ Day_8/char_triangle.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+int main()
+{
+  int rows;
+
+printf("Enter the postive number of rows: ");
+scanf("%d", &rows);
+rows=(rows<0)?-rows:rows;  
+for(int i=1; i<=rows; i++){
+    char c='A';
+for(int j=1; j<=i; j++){
+printf("%c", c);
+  c++;
+
+}
+printf("\n");
+}
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
 

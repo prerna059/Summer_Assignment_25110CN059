8e9a6a7b3a00e321105559884a0fa45113114f9a
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-08 13:20:53 +0530

    Update notriangle.c

--- Day_8/notriangle.c
+++ Day_8/notriangle.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main()
+{
+  int rows;
+printf("Enter the postive number of rows: ");
+scanf("%d", &rows);
+rows=(rows<0)?-rows:rows;  
+for(int i=1; i<=rows; i++){
+for(char j=1; j<=i; j++){
+printf("%d", j);
+}
+printf("\n");
+}
+return 0;
+}

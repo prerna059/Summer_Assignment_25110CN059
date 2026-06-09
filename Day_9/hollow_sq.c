9a58e766ddfa3d3540adb89afba67bc49dd1ea33
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-09 12:53:40 +0530

    Update hollow_sq.c

--- Day_9/hollow_sq.c
+++ Day_9/hollow_sq.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main()
+{
+  int rows;
+printf("Enter the number of rows: ");
+scanf("%d", &rows);
+  rows=(rows<0)?-rows:rows;
+ for (int i=1; i<=rows; i++){
+   for (int j=1; j<=rows; j++){
+    if(i==1 || i==rows){
+  printf("*");}
+  else{
+    if(j==1 || j==rows){ 
+    printf("*");}
+    else{printf(" ");}
+  }
+}
+  printf("\n");
+}
+return 0;
+}
+  

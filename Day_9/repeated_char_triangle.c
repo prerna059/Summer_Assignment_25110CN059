1bb6a26447a38708cd0bdb92c6638840243554b9
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-09 11:37:12 +0530

    Create repeated_char_triangle.c

---
+++ Day_9/repeated_char_triangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int rows;
+printf("Enter the postive number of rows: ");
+scanf("%d", &rows);
+rows=(rows<0)?-rows:rows;  
+for(int i=1; i<=rows; i++){
+  char c='A';
+  c++;
+   for(int j=1; j<=i; j++){
+printf("%c", c);
+   }
+printf("\n");
+}
+
+return 0;
+}

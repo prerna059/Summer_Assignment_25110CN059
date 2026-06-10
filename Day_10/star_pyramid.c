ebfe5c579f30f9abcb2e6322a727bc8bb2f3eeac
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-10 17:39:21 +0530

    Update star_pyramid.c

--- Day_10/star_pyramid.c
+++ Day_10/star_pyramid.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+int main(){
+    int size;
+    printf("Enter the size: ");
+    scanf("%d", &size);
+    size=(size<0)?-size:size;
+    for(int i=1; i<=size; i++)
+    {
+        for(int j=1;j<=size-i; j++){
+         printf(" ");
+        }
+        for (int k=1; k<=2*i-1;k++){
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+ return 0;
 
+}

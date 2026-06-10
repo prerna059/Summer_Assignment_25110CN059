39d8e1fe00df6a8943a5ebdcb7260a5ac24eecec
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-10 17:51:51 +0530

    Create sym_no_pyramid.c

---
+++ Day_10/sym_no_pyramid.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int size;
+    printf("Enter the size: ");
+    scanf("%d", &size);
+    size=(size<0)?-size:size;
+   
+    for(int i=1; i<=size; i++)
+    {   
+        int d=1;
+        int temp=d;
+        for(int j=1;j<=size-i; j++){
+         printf(" ");
+        }
+        for (int k=1; k<=(2*i-1);k++){
+           if(k<=((2*i-1)/2+1)){
+            printf("%d",d);
+            d++;
+        }
+        else if(k>((2*i-1)/2+1 && k<=2*i-1)){
+            printf("%d", temp);
+            temp++;
+        }
+    }
+        
+        printf("\n");
+    }
+ return 0;
+
+}

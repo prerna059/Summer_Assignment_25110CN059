e12c0907db8f49c8bc065f5b695fe3aad4f99e69
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-11 16:23:07 +0530

    Update sum.c

--- Day_11/sum.c
+++ Day_11/sum.c
@@ -1,1 +1,13 @@
 
+#include<stdio.h>
+int sum(int a, int b);
+int main(){
+  int a,b;
+printf("Enter the first and second numbers: ");
+  scanf("%d%d", &a, &b);
+printf("The sum of two numbers is %d", sum(a,b));
+return 0;
+}
+int sum(int a, int b){
+  return a+b;
+}

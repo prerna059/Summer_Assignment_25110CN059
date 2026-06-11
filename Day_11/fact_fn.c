7e1fc31d1b65c0cae7253d6e7e8a7971e6e62459
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-11 19:52:36 +0530

    Update fact_fn.c

--- Day_11/fact_fn.c
+++ Day_11/fact_fn.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int fact(int a);
+int main(){
+  int n;
+printf("Enter the number: ");
+scanf("%d", &n);
+  if(n<0){
+    printf("Factorial of a negative number does not exist.");
+  }
+    else{ 
+      printf("The factorial of %d is %d", n, fact(n));
+    }
+return 0;
+}
+int fact (int a){
+  if(a==0 || a==1) return 1;
+return a*fact(a-1);
+}

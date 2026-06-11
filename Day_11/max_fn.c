22cccef78f20c8c3c9053856bada725194cd64bf
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-11 19:42:36 +0530

    Update max_fn.c

--- Day_11/max_fn.c
+++ Day_11/max_fn.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int max(int a, int b);
+int main(){
+  int a,b;
+printf("Enter the first and second numbers: ");
+  scanf("%d%d", &a, &b);
+printf("The maximum of two numbers is %d", max(a,b);
+return 0;
+}
+int max(int a, int b){
+  int maximum=(a>b)?a:b;
+return maximum;
+}

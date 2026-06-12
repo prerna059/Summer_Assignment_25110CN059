d9516d3dc8c4e9a01a4544a66cec8b829e9380c6
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-12 20:53:45 +0530

    Update Armstrong_fn.c

--- Day_12/Armstrong_fn.c
+++ Day_12/Armstrong_fn.c
@@ -1,1 +1,35 @@
-
+#include<stdio.h>
+#include<math.h>
+void armstrong(int a);
+int main(){
+  int n;
+printf("Enter a number: ");
+scanf("%d", &n);
+armstrong(n);
+return 0;
+}
+void armstrong(int a){
+int temp=a, count=0, sum=0;
+     while(a!=0)
+    {
+        a=a/10;
+        count++;
+    }
+    a=temp;
+    while(a!=0)
+    {
+        int r=a%10;
+        if(r<0){
+            break;}
+        sum= sum+pow(r,count);
+        a=a/10;
+    }
+    a=temp;
+    if(sum==a || a==0){
+        printf("The entered number is armstrong");
+    }
+else{
+    printf("The entered number is not an Armstrong number");
+}
+return;
+}

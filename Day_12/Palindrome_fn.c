451e35084aec112c25f811343da4ba796b45f104
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-12 20:23:52 +0530

    Update Palindrome_fn.c

--- Day_12/Palindrome_fn.c
+++ Day_12/Palindrome_fn.c
@@ -1,1 +1,29 @@
-
+#include<stdio.h>
+void palindrome(int a);
+int main(){
+   int n;
+    printf("Enter the number :");
+    scanf("%d", &n);
+  palindrome(n);
+return 0;
+}
+void palindrome(int a){
+a=(a<0)?-a:a;
+int number=a;
+int reverse=0;
+while(a!=0)
+    {
+       int r=a%10;
+        reverse= reverse*10+r;
+        a=a/10;
+    }
+    if(number==reverse)
+    {
+        printf("The number is palindrome number.");
+    }
+    else 
+    {
+        printf("The given number is not a palindrome number.");
+    }
+    return;
+      }

f57f241408497685d568d3d178bc820183ad3d95
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-12 21:30:33 +0530

    Update fibo_fn.c

--- Day_12/fibo_fn.c
+++ Day_12/fibo_fn.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int fibo(int a);
+int main(){
+  int n;
+printf("Enter a number: ");
+scanf("%d", &n);
+  n=(n<0)?-n:n;
+   printf("The fibonacci series upto n terms:");
+for(int i=1; i<=n; i++){
+   printf("%d ", fibo(i));
+}
+return 0;
+}
+int fibo(int a){
+if(a==1) return 0;
+if(a==2) return 1;
+return fibo(a-1)+fibo(a-2);
+  }

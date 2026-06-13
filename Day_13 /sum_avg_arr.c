ee533017d7b6c6329b94ed3608387f03cf7b1c64
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-13 16:40:32 +0530

    Update sum_avg_arr.c

--- Day_13 /sum_avg_arr.c
+++ Day_13 /sum_avg_arr.c
@@ -1,1 +1,21 @@
-
+#iclude<stdio.h>
+int main(){
+  int n;
+  printf("Enter the size of array: ");
+  scanf("%d", &n);
+  int arr[n];
+  
+  for(int i=0; i<=n-1; i++){
+    printf("Enter element %d: ", i+1);
+    scanf("%d", &arr[i]);
+  }
+  int sum=0;
+  printf("Sum of array elements: ");
+  for(int j=0; j<=n-1; j++){
+    sum+=arr[j];
+  }
+   printf("%d ",sum );
+float avg = sum/n;
+printf("\nThe average is %f ", avg);
+  return 0;
+}

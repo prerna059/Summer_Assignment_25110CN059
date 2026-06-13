f742d5ad8f2b8e6a22250a66f86080ce81cc487b
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-13 20:06:21 +0530

    Update even_odd_arr.c

--- Day_13 /even_odd_arr.c
+++ Day_13 /even_odd_arr.c
@@ -5,14 +5,15 @@ int main(){
   int n;
   printf("Enter the size of Array: ");
   scanf("%d", &n);
+  n=(n<0)?-n:n;
   int arr[n];
   for(int i=0; i<=n-1; i++){
     printf("Enter element %d: ", i+1);
     scanf("%d", &arr[i]);
   }
-  
-  printf("The number of even numbers in the array is %d\n", even(n, arr));
-  printf("The number of odd elements in the array is %d", n-even(n, arr));
+  int even_no =even(n, arr);
+  printf("The number of even numbers in the array is %d\n", even_no );
+  printf("The number of odd elements in the array is %d", n-even_no);
   return 0;
 }
 int even(int a, int array[]){

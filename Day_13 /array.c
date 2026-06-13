1cf8a00409117c831b1582d9a3d467fcabc0f25a
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-13 16:27:06 +0530

    Update array.c

--- Day_13 /array.c
+++ Day_13 /array.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main(){
+  int n;
+  printf("Enter the size of array: ");
+  scanf("%d", &n);
+  int arr[n];
+  
+  for(int i=0; i<=n-1; i++){
+    printf("Enter element %d: ", i+1);
+    scanf("%d", &arr[i]);
+  }
+  for(int j=0; j<=n-1; j++){
+    printf("%d ",arr[j] );
+  }
+  return 0;
+}

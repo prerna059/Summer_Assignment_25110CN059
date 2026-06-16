dfda8eb72de3e6b73b610ba2abfd60e0f27bcd82
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-16 21:53:00 +0530

    Update reverse_arr.c

--- Day_15/reverse_arr.c
+++ Day_15/reverse_arr.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+int main(){
+     int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    if(n<0){
+        printf("Enter a positive elemnt.");
+        return 0;
+    }
+    int arr[n];
+    for(int i=0; i<n; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+ for(int i=n-1; i>=0; i--){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}

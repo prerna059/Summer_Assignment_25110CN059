0f98fe319ea6091d0e2636fa4537052529ec571c
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-21 16:49:11 +0530

    Create common.c

---
+++ Day_17/common.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+     int n1, n2;
+    printf("Enter the size of array 1 and array 2 respectively: ");
+    scanf("%d%d", &n1, &n2);
+    //negative input handle
+    if(n1<0 || n2<0){
+        printf("Invalid inputs.");
+        return 0;
+    }
+    //arrays input
+    int arr1[n1];
+    for(int i=0; i<n1; i++) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr1[i]);
+    }
+     int arr2[n2];
+     for(int i=0; i<n2; i++) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr2[i]);
+      }
+    //common elements store 
+      int n=(n1<n2)?n1:n2;
+      for(int j=0; j<n; j++) {    
+            if(arr1[j] == arr2[j]) {
+                printf("%d ", arr1[j];           
+            }  
+        }
+    }
+   return 0;
+}      
+           

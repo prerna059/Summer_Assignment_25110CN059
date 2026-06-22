16ff42e3e62e99e9979debd250005224aeba5a03
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-22 22:10:57 +0530

    Add files via upload

---
+++ Day_18/bubblesort.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+void array_input (int n, int arr[]);
+void array_print (int n, int result_arr[]);//with frequency of one kind =1
+int main() 
+{
+    int n;
+    printf("Enter array size: ");
+    scanf("%d", &n);
+    if(n<0){
+        printf("Enter a positive element.");
+        return 0;
+    }
+
+    int arr[n];
+    array_input (n, arr);
+ 
+    for(int i = 1; i<n; i++) {
+        for(int j = 0; j<=n-i-1; j++) {
+               if(arr[j] > arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j + 1];
+                    arr[j + 1]=temp;
+               }
+            }
+        }
+
+     array_print(n, arr);            
+return 0;
+}
+void array_input (int n, int arr[]) 
+{
+   for(int i = 0; i<n; i++) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    return;
+}
+void array_print (int n, int result_arr[]) 
+{
+    printf("%d ", result_arr[0]);
+    for(int j = 1; j < n; j++) {
+        for(int k = 0; k < j; k++) {
+            if(result_arr[j] == result_arr[k]) {
+                break;
+            }
+            else if(result_arr[j]!= result_arr[k] && k==j-1) {
+        printf("%d ", result_arr[j]);
+           }
+        }
+    }
+ return; 
+}

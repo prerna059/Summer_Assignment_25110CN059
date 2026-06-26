f624e9aa72b93bc61f697fb9c10fe09f1492c213
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-26 23:32:58 +0530

    Update strlen.c

--- Day_21/strlen.c
+++ Day_21/strlen.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+  int n;
+  printf("Enter a size for string: ");
+  scanf("%d", &n);
 
+  if(n<=0){
+    printf("Entered value is not correct.");
+    return 0;
+  }
+
+    char str[n];
+    printf("Enter a string: ");
+    scanf(" %[^\n]", str); // after scanfing 'n', dis scanf reads the \n newline char in input buffer & leads to be taken as input (so we add a space to avoid it) 
+   int count=0;
+while(str[count] != '\0'){
+        count++;
+    }
+printf("Length of the string is: %d", count);
+
+    return 0;
+}

82399896d70882957ed615ec7610ea05c10b7ee4
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-28 22:57:45 +0530

    Update Q4.c

--- Day_23/Q4.c
+++ Day_23/Q4.c
@@ -1,1 +1,30 @@
+#include<stdio.h.
+#include<string.h>
+int main() {
+    char str[500];
+    int freq[256] = {0};   
+    int max = 0;
+    unsigned char maxChar;
 
+   puts("Enter first string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; 
+    
+    for(int i = 0; str[i] != '\0'; i++){
+        unsigned char ch = str[i];
+        freq[ch]++;
+      if(freq[ch]>max){
+            max = freq[ch];
+            maxChar=ch;
+        }
+    }
+
+    if(max>0){
+        printf("Maximum occurring character: %c", maxChar);
+    }
+    else{
+        printf("Empty string");
+    }
+
+    return 0;
+     }

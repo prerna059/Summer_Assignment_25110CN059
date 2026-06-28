7b59e2d6453dd15a55b7a26551df6ea9b8e67784
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-28 22:46:57 +0530

    Update Q3.c

--- Day_23/Q3.c
+++ Day_23/Q3.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[500], str2[500];
+    int freq[256] = {0};  
+
+    puts("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+    str[strcspn(str1, "\n")] = '\0'; 
+  
+    puts("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+    str[strcspn(str2, "\n")] = '\0'; 
+    
+    for(int i=0; str1[i] != '\0'; i++){
+        unsigned char ch = str1[i];
+        freq[ch]++;
+    }
+
+    for(int i= 0; str2[i] != '\0'; i++){
+        unsigned char ch = str2[i];
+        freq[ch]--;
+    }
+
+    for(int i = 0; i < 256; i++){
+        if (freq[i] != 0) {
+            printf("The strings are not anagrams.");
+            return 0;
+        }
+    }
+
+    printf("The strings are anagrams.");
+    return 0;
+}
 

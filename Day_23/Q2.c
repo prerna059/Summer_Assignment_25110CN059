2ac3d9b6189dbfea17e62096a9d1be5968b043c5
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-28 22:14:30 +0530

    Update Q2.c

--- Day_23/Q2.c
+++ Day_23/Q2.c
@@ -1,1 +1,28 @@
 //Find first repeating character
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[500];
+    int freq[256] = {0}; //256 ascii characters in c.
+
+    puts("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; 
+    
+    for(int i=0; str[i] != '\0'; i++){
+        unsigned char ch = str[i];//If a signed char as array index (freq[ch]),& char has a value>127(extended ASCII) or <0, it can produce a negative index>undefined behavior, memory corruption
+        freq[ch]++;
+    }
+
+   for(int i=0; str[i] != '\0'; i++){
+        unsigned char ch = str[i];
+        if(freq[ch] > 1){
+            printf("First repeating character: '%c'", ch);
+            return 0;
+        }
+         }
+
+    printf("No repeating character found.");
+    return 0;
+        }

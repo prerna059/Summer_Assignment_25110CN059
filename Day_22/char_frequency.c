c5be8d26dd2326479bd46180ae47975b5395ae15
PRERNA LODHI <prernalodhi2006@gmail.com>
2026-06-28 21:49:30 +0530

    Update char_frequency.c

--- Day_22/char_frequency.c
+++ Day_22/char_frequency.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200];
+    int freq[256] = {0}; //256 ascii characters in c.
+
+    puts("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; 
+    
+    for(int i=0; str[i] != '\0'; i++) {
+        unsigned char ch = str[i];  //no negatives.
+        freq[ch]++;
+    }
+
+    printf("Character frequencies:\n");
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > 0) {
+            printf("'%c': %d\n", i, freq[i]);
+        }
+    }
+
+    return 0;
+}
 
